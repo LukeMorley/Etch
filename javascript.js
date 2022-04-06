1629339c1944a23ea6d3ace88609a15aa2e1310c
Luke Morley <luke.morley@hotmail.com>
2022-04-06 22:46:22 +1000

    Added some comments & Rainbow Mode

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Grab all the HTML elements we will be using to change behaviours
 const grid = document.getElementById("gridContainer");
 const slider = document.getElementById("gridSizeSlider");
 const value = document.getElementById("sizeVariable");
@@ -6,13 +7,19 @@ const colorPicker = document.getElementById("colorpicker");
 const clearButton = document.getElementById("clearGrid");
 const classicButton = document.getElementById("hover");
 const clickButton = document.getElementById("click");
+const rainbowButton = document.getElementById("rainbow");
 
+//Setup default values
 const DEFAULTCOLOR = '#000000';
 const gridWidth = 600;
 let currentColor = DEFAULTCOLOR;
 let mode = 'click&drag';
-
 let mouseDown = false;
+let rainbowMode = false;
+let gridSize = 32;
+
+//Set functions to buttons & mouse behaviour when dragging outside the
+//paintable area
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 window.onmouseup = () => (mouseDown = false);
@@ -21,34 +28,67 @@ colorPicker.onchange = (e) => currentColor = e.target.value;
 button.onclick = () => clearGrid();
 classicButton.onclick = () => setMode('classic');
 clickButton.onclick = () => setMode('click&drag');
+rainbowButton.onclick = () => toggle();
 clearButton.onclick = () => clearGrid();
 
-let gridSize = 32;
+//Turn Rainbow mode on or off
+function toggle(){
+    if(!rainbowMode){
+        rainbowMode = true;
+        rainbowButton.classList.add('active');
+    }else{
+        rainbowMode = false;
+        rainbowButton.classList.remove('active');
+    }
+}
 
+//Set the text above the grid size slider upon changing
 slider.oninput = function(){
     value.innerHTML=this.value+' X '+this.value;
     gridSize=this.value;
 }
 
+//Generate a random Hex colour for Rainbow Mode
+let randomHex = () => {
+    let i = (Math.random()* 0xfffff * 1000000).toString(16);
+    return '#'+i.slice(0,6);
+}
+
+//Clear current grid & create a new one
 function clearGrid(){
     grid.innerHTML='';
     createGrid(gridSize);
 }
 
+//Paint the current tile depending on the current settings
 function paint(e){
     if(mode=='classic'){
+        if(rainbowMode){
+            newColor = randomHex();
+            this.style.backgroundColor = newColor;
+            colorPicker.value = newColor;
+            return;
+        }
         this.style.backgroundColor = currentColor;
-    }else{
-        if (e.type === 'mouseover' && !mouseDown) return
+    }else if(mode=='click&drag'){
+        if (e.type === 'mouseover' && !mouseDown) return;
+        if(rainbowMode){
+            newColor = randomHex();
+            this.style.backgroundColor = newColor;
+            colorPicker.value = newColor;
+            return;
+        }
         this.style.backgroundColor = currentColor;
     }
 }
 
+//Generate the initial grid
 window.onload = function(){
     createGrid(gridSize);
     clickTheButton('click&drag');
 }
 
+//Generate a new grid
 function createGrid(gridSize){
     let cellSize = gridWidth/gridSize;
     for(let i = 0; i<gridSize; i++){
@@ -69,11 +109,13 @@ function createGrid(gridSize){
     }
 }
 
+//Helper function to update the mode var
 function setMode(newMode){
     clickTheButton(newMode);
     mode = newMode;
 }
 
+//Toggle between classic or click mode
 function clickTheButton(newMode){
     if(mode=='click&drag'){
         clickButton.classList.remove('active');
